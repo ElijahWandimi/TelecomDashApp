939abdd80112bbd78a84191bf8590e98bd37b042
Elijah Wandimi <wandimiem7@gmail.com>
2021-07-27 13:37:25 +0300

    updates

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
